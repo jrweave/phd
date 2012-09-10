ad5dbc281d6022fd36391fff4502e852bdc0ed27
Jesse Weaver <jrweave@gmail.com>
2012-09-10 15:37:05 -0400

    Added licensing and readmes.

--- ucs/scripts/index.pl
+++ ucs/scripts/index.pl
@@ -1,6 +1,20 @@
 #!/usr/bin/perl
 
-# I HEREBY FORMALLY APOLOGIZE TO ANYONE WHO HAS TO LOOK AT THIS CODE.
+# Copyright 2012 Jesse Weaver
+# 
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+# 
+#        http://www.apache.org/licenses/LICENSE-2.0
+# 
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+#    implied. See the License for the specific language governing
+#    permissions and limitations under the License.
+
+# It's a mess, but it works.
 
 $CODE_VALUE = 0;
 $CHAR_NAME = 1;
